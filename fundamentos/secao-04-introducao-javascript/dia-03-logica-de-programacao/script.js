e7f848cf1297a5c0da005cb72029648daf51131a
jesue <jesue_gege@hotmail.com>
2022-09-23 17:11:27 -0300

    1.2 e 1.3 finalizada

--- fundamentos/secao-04-introducao-javascript/dia-03-logica-de-programacao/script.js
+++ fundamentos/secao-04-introducao-javascript/dia-03-logica-de-programacao/script.js
@@ -6,3 +6,32 @@
 //    factorial = factorial * (index - 1)
 // }
 // console.log(factorial)
+
+///~///~///~///~///~///~///~///~///~///~///~///~///~///~///~///~///~///
+
+// exercicios 1 (1.2)
+
+// let word = 'pedro';
+// let drow = '';
+
+// for (let index = word.length -1; index >=0; index -= 1){
+//     drow += word[index]
+// }
+// console.log(drow)
+
+///~///~///~///~///~///~///~///~///~///~///~///~///~///~///~///~///~///
+
+// exercicios 1 (1.3)
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorPalavra = '';
+
+// for (index = 0; index < array.length; index += 1){
+//  if(maiorPalavra.length < array[index].length){
+//     maiorPalavra = array[index]
+//  }
+// }
+// console.log(maiorPalavra)
+
+// exercicios 1 (1.4)
+
