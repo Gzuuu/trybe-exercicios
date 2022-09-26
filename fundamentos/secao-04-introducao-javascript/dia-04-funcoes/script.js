fb93c21796a54084f91780cb948a90417ea00e00
jesue <jesue_gege@hotmail.com>
2022-09-26 18:51:52 -0300

    exercicios do 1 ao 1.4

---
+++ fundamentos/secao-04-introducao-javascript/dia-04-funcoes/script.js
@@ -0,0 +1,49 @@
+// exercicios 1 (1.1)
+
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   console.log('Bem-vinda(o), ' + info.personagem)
+
+// exercicios 1 (1.2)
+
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   info['recorrente'] = 'sim';
+
+//   console.log(info)
+
+// exercicios 1 (1.3)
+
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   info['recorrente'] = 'sim';
+
+// for (let key in info){
+//     console.log(key)
+// }
+
+// exercicios 1 (1.4)
+
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   info['recorrente'] = 'sim';
+
+//   for (let key in info){
+//     console.log(info[key])
+//   }
