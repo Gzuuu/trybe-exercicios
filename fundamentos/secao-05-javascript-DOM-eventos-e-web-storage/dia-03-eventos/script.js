898c663ee6d040f84c05ccf4cbe0b9243aff7c01
jesue <jesue_gege@hotmail.com>
2022-09-30 16:04:33 -0300

    parte 2 concluida

--- fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/dia-03-eventos/script.js
+++ fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/dia-03-eventos/script.js
@@ -14,6 +14,7 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  // parte 1 //
   const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   for(let index = 0; index < decemberDaysList.length; index += 1){
@@ -27,4 +28,11 @@ function createDaysOfTheWeek() {
     if (decemberDaysList[index] === 4 || decemberDaysList[index] === 11 || decemberDaysList[index] === 18 || decemberDaysList[index] === 25){
         lis.className += ' friday'
     }
+  }
+  // parte 2 // 
+
+  function buttonCreate (string){
+    let button = document.createElement('button')
+    button.setAttribute('id', 'btn-holiday')
+    document.querySelector('.buttons-container').appendChild(button)
   }
