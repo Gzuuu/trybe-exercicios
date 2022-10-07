75d1c1968e5374f31782d241878188bed8ad3f1f
jesue <jesue_gege@hotmail.com>
2022-10-07 15:12:29 -0300

    adiciona-pasta

---
+++ fundamentos/secao-06-HTML-CSS-Forms-Flexbox-responsive/dia-02-bibliotecas-e-frameworks/script.js
@@ -0,0 +1,15 @@
+let botao = document.querySelector('#btn-submit')
+function prevent (){
+    let name = document.querySelector('#name-content').value
+    let email = document.querySelector('#email-content').value
+    if(name.length < 10 || name.length > 40){
+        alert('Dados inválidos')
+    }else if (email.length < 10 || email.length > 40){
+        alert('Dados inválidos')
+    } else alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+}
+
+botao.addEventListener('click', function(event){
+    event.preventDefault()
+    prevent()
+})
